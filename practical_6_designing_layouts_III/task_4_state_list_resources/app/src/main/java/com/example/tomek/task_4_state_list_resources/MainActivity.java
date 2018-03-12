6038bd0f636040bea0b3f301d625e686ca224012
Tomasz Demczuk <rainbowvme@gmail.com>
2018-03-12 12:56:33 +0000

    TODO task 5

--- practical_6_designing_layouts_III/task_4_state_list_resources/app/src/main/java/com/example/tomek/task_4_state_list_resources/MainActivity.java
+++ practical_6_designing_layouts_III/task_4_state_list_resources/app/src/main/java/com/example/tomek/task_4_state_list_resources/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tomek.task_4_state_list_resources;
 
+import android.graphics.drawable.StateListDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
